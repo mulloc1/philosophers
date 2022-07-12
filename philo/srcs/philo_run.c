c094e566f1acd402464cd283ae6b0435d871e038
Jaemin Bae <jaebae@c5r4s5.42seoul.kr>
2022-07-12 16:13:09 +0900

    1 philosophers taken fork

--- philo/srcs/philo_run.c
+++ philo/srcs/philo_run.c
@@ -6,7 +6,7 @@
 /*   By: jaebae <jaebae@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 09:34:11 by jaebae            #+#    #+#             */
-/*   Updated: 2022/07/12 09:34:35 by jaebae           ###   ########.fr       */
+/*   Updated: 2022/07/12 16:00:26 by jaebae           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*philo_run(void *ptr)
 	t_philo	*philo;
 
 	philo = (t_philo *)ptr;
-	if (philo->info->number_to_philo == 1)
-		ft_usleep(philo->info->time_to_eat_u * 2);
 	if (philo->left_fork % 2 == 0)
 		ft_usleep(philo->info->time_to_eat_u);
 	while (philo->info->is_processing)
